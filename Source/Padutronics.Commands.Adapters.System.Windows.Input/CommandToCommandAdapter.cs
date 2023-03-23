febe8f9c737fa149c192a25825c97e30871885d1
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 12:55:35 +0100

    Add command adapter

---
+++ Source/Padutronics.Commands.Adapters.System.Windows.Input/CommandToCommandAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Padutronics.Commands.Adapters.System.Windows.Input;
+
+public sealed class CommandToCommandAdapter : global::System.Windows.Input.ICommand
+{
+    private readonly ICommand adaptee;
+
+    public CommandToCommandAdapter(ICommand adaptee)
+    {
+        this.adaptee = adaptee;
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add
+        {
+            if (value is not null)
+            {
+                adaptee.CanExecuteChanged += new EventHandler<CanExecuteChangedEventArgs>(value);
+            }
+        }
+        remove
+        {
+            if (value is not null)
+            {
+                adaptee.CanExecuteChanged -= new EventHandler<CanExecuteChangedEventArgs>(value);
+            }
+        }
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return adaptee.CanExecute();
+    }
+
+    public void Execute(object? parameter)
+    {
+        adaptee.Execute();
+    }
+}
